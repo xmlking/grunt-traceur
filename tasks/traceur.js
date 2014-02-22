d0e51bfa9ea0773d856310a7ecdcad38576c04b8
Sumo <xmlking+github@gmail.com>
2014-02-21 19:19:27 -0800

    supports traditional way to set source and dest.
      grunt.initConfig({
    
        traceur: {
          options: {
              // traceur options here
              experimental: true,
              blockBinding: true,
              deferredFunctions: true,
              annotations: true,
              debug:true
          },
          custom: {
              files:[{
                  expand: true,
                  cwd: '<%= yeoman.app %>/modules/',
                  src: ['**/*.es6.js'],
                  dest: '<%= yeoman.app %>/modules/',
                  ext: '.js'
              }]
          }
        },...
    
    
        // Watches files for changes and runs tasks based on the changed files
        watch: {
          traceur :  {
            files :  [ '<%= yeoman.app %>/modules/**/*.es6.js' ],
            tasks :  [ 'newer:traceur' ]
          }, ....

--- tasks/traceur.js
+++ tasks/traceur.js
@@ -23,69 +23,34 @@ function asyncCompile(content, filename, options, callback) {
   callback(null, result);
 }
 
+
 /**
 * Compiles a list of srcs files
 * */
 function compileAll(grunt, compile, group, options, callback) {
-
-
-  var srcs = group.src, dest = group.dest;
-    grunt.log.debug('Compiling... ' + dest);
-
+var srcs = group.src, dest = group.dest;
+  grunt.log.debug('Compiling... ' + dest);
 
   async.map(srcs, function(src, callback) {
     var content = grunt.file.read(src).toString('utf8');
-    grunt.log.debug("Reading file " + src);
     compile(content, src, options, callback);
   }, function(err, result) {
     if (err) {
       grunt.log.error(err);
       callback(false);
     } else {
-      result.map( function(res) {
-
-            console.log("Group",group, "srcs", srcs, "dest", dest);
-          console.log("test:", dest + path.basename(res.file));
-          if(!grunt.file.exists(dest )) {
-            grunt.file.mkdir(dest);
-          }
-          else if(grunt.file.isDir(dest) ){
-
-             grunt.log.error("Destination exists and is NOT a directory");
-          }
-          if(res.errors && res.errors.length > 0) {
-            grunt.log.error("Errors encountered for " + res.file);
-            grunt.log.error(res.errors );
-
-          }
-          else {
-              var fileName = path.basename(res.file),
-                  filePath = dest + fileName,
-                  mapName = fileName + ".map",
-                  mapPath = dest + mapName,
-                  sourceMap = res.sourceMap;
-              res.js += "//# sourceMappingURL=" + mapName;
-
-              grunt.file.write(filePath, res.js, {encoding: 'utf8'});
-              if(res.sourceMap) {
-                  sourceMap = JSON.parse(res.sourceMap);
-                  sourceMap.file =  fileName;
-                  sourceMap = JSON.stringify(sourceMap);
-                  grunt.file.write(mapPath,  sourceMap , {encoding: 'utf8'});
-                  grunt.file.copy(res.file, filePath +'.es6');
-              }
-
-
-
-              grunt.log.debug('Compilation successful - ' + dest);
-
-          }
-
-      });
-
-      grunt.log.ok(srcs + ' -> ' + dest);
-      callback(true);
-    }
+		result.map( function(res) {
+			if(res.errors && res.errors.length > 0) {
+            	grunt.log.error("Errors encountered for " + res.file);
+            	grunt.log.error(res.errors );
+          	} else {
+          		grunt.log.debug('Compilation successful - ' + dest);
+          		grunt.file.write(dest, res.js, {encoding: 'utf8'});
+          	}
+		});
+		grunt.log.ok(srcs + ' -> ' + dest);
+        callback(true);
+      }
   });
 }
 
@@ -96,7 +61,7 @@ module.exports = function(grunt) {
         sourceMaps: false,
         spawn: true
       });
-      grunt.log.write('using options: ' + JSON.stringify(options));
+      grunt.log.debug('using options: ' + JSON.stringify(options));
       var done = this.async();
       var compile = asyncCompile;
 
